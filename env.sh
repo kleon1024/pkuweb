836397a52893a4e3f27b9ad06ea47a493f100db6
Ding Li <1995dingli@gmail.com>
2020-03-22 18:49:19 +0800

    Add deploy environment

---
+++ env.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install Docker-CE 
+apt update
+apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt update
+apt-cache policy docker-ce
+apt install -y docker-ce
+systemctl status docker
+
+# Install Docker-Compose
+curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
