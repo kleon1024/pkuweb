a1f6951e8dc0b4f6ecb6342df8e67ae6d7e15892
Ding Li <1995dingli@gmail.com>
2020-08-05 09:43:25 +0800

    Update store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -352,11 +352,11 @@ export default new Vuex.Store({
         "randomOrder",
       ],
       // Enable encription
-      storage: {
-        getItem: (key) => ls.get(key),
-        setItem: (key, value) => ls.set(key, value),
-        removeItem: (key) => ls.remove(key),
-      },
+      // storage: {
+      //  getItem: (key) => ls.get(key),
+      //  setItem: (key, value) => ls.set(key, value),
+      //  removeItem: (key) => ls.remove(key),
+      // },
     })
   ]
 });
