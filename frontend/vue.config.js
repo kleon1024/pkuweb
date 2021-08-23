3ef49d137e0819748d64290f4903bb572b70dbf6
Ding Li <1995dingli@gmail.com>
2021-08-23 10:11:12 +0800

    replace http -> https

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -70,7 +70,7 @@ module.exports = {
       // 设置代理
       // proxy all requests starting with /api to jsonplaceholder
       "/api": {
-        target: "https://api.pkuzhiyuan.com",
+        target: "http://api.pkuzhiyuan.com",
         changeOrigin: true,
         ws: true,
         pathRewrite: {
