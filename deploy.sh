b3fc163dab56aa0900718a30b1ce2b2ef80b7796
li.ding <li.ding@bytedance.com>
2025-02-27 22:31:48 +0800

    add more

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,10 @@ docker-compose -f nginx-compose.yml up -d
 docker-compose -f deploy-compose.yml pull
 if [ ! -d "mongodb" ]; then
     docker-compose -f deploy-compose.yml up -d
-    sleep 5s
+    sleep 5
     echo "initializing mongodb"
-    docker exec pkuweb_mongo_1 mongoimport --username root --password tCaJXhP77xdKR4Y7 \
+    docker exec pkuweb-mongo-1 mongoimport --username root --password tCaJXhP77xdKR4Y7 \
+        --authenticationMechanism SCRAM-SHA-256 \
         -d nx_zhiyuan -c users --file /init/nx_students.json --jsonArray
 else
     echo "found existed mongodb"
